32417a19f65b466829586a77fdda73d492b5922e
Krishna Jhapate <ki54.jhapate@gmail.com>
2024-05-11 13:18:29 +0530

    constants

---
+++ src/config/constants.ts
@@ -0,0 +1,5 @@
+export const PORT = process.env.PORT || 5000;
+export const JWT_SECRET = process.env.JWT_SECRET || "SECRET";
+export const USERNAME = process.env.USERNAME;
+export const PASSWORD = process.env.PASSWORD;
+export const MAX_FILE_SIZE = 10 * 1024 * 1024; // 10MB

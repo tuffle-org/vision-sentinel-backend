13fc90e9b7d7afd6d295d075701629d9fefed1ae
Krishna Jhapate <ki54.jhapate@gmail.com>
2024-05-11 13:18:48 +0530

    ws setup

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import express, { Response } from "express";
 import http from "http";
 import WebSocket from "ws";
-import { PrismaClient } from "@prisma/client";
+import { PORT } from "./config/constants";
+import { loginUser } from "./controllers/auth";
 
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
-const prisma = new PrismaClient();
 
 app.use(express.json());
+
+app.post("/api/login", loginUser);
 
 wss.on("connection", (ws: WebSocket) => {
     ws.on("message", async (message: string) => {
@@ -20,15 +22,6 @@ wss.on("connection", (ws: WebSocket) => {
     });
 });
 
-app.get("/users", async (_, res: Response) => {
-    try {
-        const users = await prisma.user.findMany();
-        res.json(users);
-    } catch (error) {
-        res.status(500).json({ error: "Internal Server Error" });
-    }
-});
-
-server.listen(3000, () => {
-    console.log("Server running on port 3000");
+server.listen(PORT, () => {
+    console.log(`Server running on port http://localhost:${PORT}`);
 });

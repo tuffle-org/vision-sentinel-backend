06caed6459d87444d30dc53f67207353347ed5b1
Krishna Jhapate <ki54.jhapate@gmail.com>
2024-05-13 00:53:03 +0530

    remove code clean

--- src/main.ts
+++ src/main.ts
@@ -12,41 +12,18 @@ import {
     updateInOut,
     updateUser,
 } from "./controllers/users";
-import multer from "multer";
-import path from "path";
 
 const app = express();
 const server = http.createServer(app);
 export const wss = new WebSocket.Server({ server });
 
-// Define storage options for Multer
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        // Specify the destination directory for uploaded files
-        cb(null, "uploads/");
-    },
-    filename: function (req, file, cb) {
-        // Use the original file name as the uploaded file name
-        cb(null, file.originalname);
-    },
-});
-
-// Initialize multer with storage settings
-const upload = multer({ storage: storage });
-
 app.use(express.json());
-app.use("/static", express.static(path.join(__dirname, "../uploads")));
 
 setupWebSocketServer(wss);
 
 app.post("/api/login", loginUser);
 
-app.post(
-    "/api/create-user",
-    authMiddleware,
-    upload.single("user_image"),
-    createUser
-);
+app.post("/api/create-user", authMiddleware, createUser);
 app.get("/api/get-user", authMiddleware, getUser);
 app.post("/api/update-user/:id", authMiddleware, updateUser);
 app.delete("/api/delete-user/:id", authMiddleware, deleteUser);

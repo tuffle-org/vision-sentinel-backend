72bf3cd8b7c113b9b47c5c60c55c736dfb92f17f
Krishna Jhapate <ki54.jhapate@gmail.com>
2024-05-12 16:58:30 +0530

    user function update and create

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -30,7 +30,7 @@ export async function createUser(req: Request, res: Response) {
             data: {
                 user_id,
                 user_name,
-                user_image: user_image.filename,
+                user_image: "/static/" + user_image.filename,
                 group,
                 expiry_date,
                 created_at: new Date(),
@@ -61,17 +61,13 @@ export async function getUser(req: Request, res: Response) {
 
 export async function updateUser(req: Request, res: Response) {
     try {
-        const {
-            user_id,
-            user_name,
-            group,
-            expiry_date,
-            user_status,
-            face_data,
-        } = req.body as any;
+        const { user_id, user_name, group, expiry_date, face_data } =
+            req.body as any;
+
+        const userId: string = req.query.id as string;
 
         // Validate user data (add more validation as needed)
-        if (!user_id || !user_name || !group || !expiry_date) {
+        if (!user_name || !group || !expiry_date) {
             return res.status(400).json({ error: "Missing required fields" });
         }
 
@@ -82,25 +78,64 @@ export async function updateUser(req: Request, res: Response) {
         if (!existingUser) {
             return res.status(404).json({ error: "User not found" });
         }
+        const updateObj: any = {
+            user_name,
+            group,
+            expiry_date,
+            face_data,
+        };
 
-        const userId: string = req.query.id as string;
+        const user_image = req.file; // Access the uploaded file buffer
+
+        // Check if user_image is not undefined
+        if (user_image) {
+            updateObj.user_image = "/static/" + user_image.filename;
+        }
 
         // Update user in the database using Prisma
         const updatedUser = await prisma.user.update({
-            where: { id: userId },
-            data: {
-                user_name,
-                group,
-                expiry_date,
-                updated_at: new Date(),
-                user_status: user_status || "Registration",
-                face_data,
-            },
+            where: { user_id: userId },
+            data: updateObj,
         });
 
         res.status(200).json(updatedUser);
     } catch (error) {
         console.error("Error updating user:", error);
         res.status(500).json({ error: "Internal server error" });
+    }
+}
+
+export async function updateInOut(req: Request, res: Response) {
+    try {
+        console.log(req.query, req.params, req.body);
+        // Extract userId and status from request
+        const userId: string = req.params.id as string;
+        const { status }: { status: "In" | "Out" } = req.body;
+
+        // Check if userId and status are present
+        if (!userId || !status) {
+            return res.status(400).json({ error: "Missing required fields" });
+        }
+
+        // Check if user exists
+        const existingUser = await prisma.user.findFirst({
+            where: { user_id: userId },
+        });
+        if (!existingUser) {
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        // Update user's status in the database using Prisma
+        const updatedUser = await prisma.user.update({
+            where: { user_id: userId },
+            data: { user_status: status },
+        });
+
+        // Return the updated user as a response
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        // Handle errors
+        console.error("Error updating user:", error);
+        res.status(500).json({ error: "Failed to update user status" });
     }
 }
